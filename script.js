b923175e127de544658e9cbfc2858e9c640ffa50
Tranquil Tortoise <SlinkTheTranquilTortoise@gmail.com>
2025-07-01 11:31:46 -1000

    add header and display library button; add styling

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function Book(title, author, qtyPages, haveRead) {
     if (!new.target) {
         throw Error("did not use 'new' operator");
     }
+    this.id = self.crypto.randomUUID();
     this.title = title;
     this.author = author;
     this.qtyPages = qtyPages;
@@ -16,8 +17,6 @@ function Book(title, author, qtyPages, haveRead) {
 }
 
 const theHobbit = new Book('The Hobbit', 'JRR Tolkien', 295, false);
-
 const warAndPeace = new Book('War and Peace', 'Tolstoy', 875, true);
-
-console.log(theHobbit.info());
-console.log(warAndPeace.info());
+const catInTheHat = new Book('Cat in the Hat','Dr. Seuss', 23, true);
+const noTreason = new Book('No Treason', 'Spooner', 69, false);
